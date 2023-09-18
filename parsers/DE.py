0a0c643503686fb2614169dac695e88d8a98bdc1
Silke Bonnen <silke.bonnen@gmail.com>
2023-09-18 14:24:02 +0200

    name and comment changes

--- parsers/DE.py
+++ parsers/DE.py
@@ -4,12 +4,8 @@ from typing import Any
 import re
 import json
 import arrow
-
 from pytz import timezone
-
-# The request library is used to fetch content through HTTP
 from requests import Response, Session
-
 from electricitymap.contrib.lib.models.event_lists import (
     ProductionBreakdownList,
 )
@@ -17,25 +13,16 @@ from electricitymap.contrib.lib.models.events import ProductionMix, StorageMix
 from electricitymap.contrib.lib.types import ZoneKey
 from parsers.lib.exceptions import ParserException
 
-'''
-    I used pattern matching because the json file that the website receives from a backend, held the data in some 
-    JavaScript object, so the json that was holding the data also contained method calls, e.g. showconventionalSeries, 
-    so when trying to decode the json with json.loads() it gave errors. I would have looked into doing this another 
-    way if I had more time.
-
-    The data also had data about conventional power but I didn't know where to add it, so I didn't use it.
-    I don't know if it is correct but I added coal and lignite data to coal.
-'''
-
+# There was also data about conventional power but I didn't know what to add it to, so I didn't use it.
 TYPE_MAPPING = {
     r'{"id":"solar".*?]}': "solar", 
     r'{"id":"biomass".*?]}': "biomass",
-    r'{"id":"coal".*?]}': "coal",
     r'{"id":"gas".*?]}': "gas",
     r'{"id":"run-of-the-river".*?]}': "hydro",
     r'{"id":"uranium".*?]}': "nuclear",
-    r'{"id":"wind-onshore".*?]}': "wind",
+    r'{"id":"wind-onshore".*?]}': "wind",       # wind-onshore and wind-offshore is added to wind
     r'{"id":"wind-offshore".*?]}': "wind",
+    r'{"id":"coal".*?]}': "coal",               # coal and lignite data is added to coal
     r'{"id":"lignite".*?]}': "coal", 
 }
 
@@ -46,12 +33,12 @@ def fetch_production(
     logger: Logger = getLogger(__name__),
 ) -> list[dict] | dict:
 
-    #Requests the last known production mix (in MW) of a given country.
+    # Requests the last known production mix (in MW) of a given country.
+    
     if target_datetime:
         raise NotImplementedError("This parser is not yet able to parse past dates")
 
     datetime_today = datetime.today().strftime("%d.%m.%Y")
-
     url = f"https://www.agora-energiewende.de/en/service/recent-electricity-data/chart/data/power_generation/{datetime_today}/{datetime_today}/today/chart.json"
     
     res: Response = session.get(url)
@@ -69,14 +56,14 @@ def fetch_production(
     #logger.debug(f"Raw generation breakdown: {obj}")
 
     series_pattern = r'"series":\[.*\]'
-    match = re.search(series_pattern, obj["js"])
-    json_string = match.group()
-    pat = r'\[.+\]'
-
+    series_match = re.search(series_pattern, obj["js"])
+    series_string = series_match.group()
+    
+    data_pattern = r'\[.+\]'
     for pattern, mode in TYPE_MAPPING.items():
-        match = re.search(pattern, json_string)
-        group = match.group()
-        data_match = re.search(pat, group)
+        data_match = re.search(pattern, series_string)
+        data_string = data_match.group()
+        data_match = re.search(data_pattern, data_string)
         json_data = json.loads(data_match.group())
         last_data_point = json_data[-1][1]
         production_mix.add_value(mode, last_data_point)
@@ -93,5 +80,5 @@ def fetch_production(
 if __name__ == "__main__":
     """Main method, never used by the Electricity Maps backend, but handy for testing."""
 
-    print("fetch_production(XX) ->")
-    print(fetch_production(ZoneKey("XX")))
+    print("fetch_production(DE) ->")
+    print(fetch_production(ZoneKey("DE")))
